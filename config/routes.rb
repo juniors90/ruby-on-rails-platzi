b893922c6dd0a0fb0464bb127d0a91591489273e
juniors90 <juandavid9a0@gmail.com>
2022-06-26 14:43:35 -0300

    Entender el enrutamiento básico

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'hola', to: "welcome#hello"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
